cd00ce669275c1a46cc05c8ab7eb20e14e1e8684
GrigoryTsuryev <tsurievga@gmail.com>
2017-10-24 15:59:19 +0300

    first commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,21 @@
+package sample;
+
+import javafx.scene.control.TextField;
+import javafx.fxml.FXML;
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+
+public class Controller {
+    @FXML
+    private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
+
+    @FXML
+    public void onButtonClicked(){
+
+    }
+}
